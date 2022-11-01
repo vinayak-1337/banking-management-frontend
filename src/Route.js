538e376943d45255e5f213a5464134ca90c42eff
vinayak <vinayak@gkmit.co>
2022-11-01 18:15:39 +0530

    Change dashboard and login routes

--- src/Route.js
+++ src/Route.js
@@ -6,12 +6,14 @@ import { UserContext } from "./context/user.context";
 import Navigations from "./components/navigations/navigation.component";
 import RegisterForm from "./components/register-form/register-form.component";
 import LoginForm from "./components/login-form/login-form.component";
-import UserDashboard from "./routes/user-dashboard/user-dashboard.component";
+import Dashboard from "./routes/dashboard/dashboard.component";
 import MoneyTransfer from "./components/money-transfer/money-transfer.component";
 import DepositForm from "./components/deposit-form/deposit-form.component";
 import FundTransfer from "./routes/fund-transfer/fund-transfer.component";
 import Balance from "./components/balance/balance.component";
 import Profile from "./components/profile/profile.components";
+import Login from "./routes/login/login.component";
+import Register from "./routes/register/register.component";
 
 function MainRoutes() {
   const { currentUser } = useContext(UserContext);
@@ -24,12 +26,12 @@ function MainRoutes() {
           <Route index element={<UserDashboard />} />
         )} */}
         <Route index element={<Home />} />
-
-        <Route path="register" element={<RegisterForm />} />
-        <Route path="login" element={<LoginForm />} />
+        <Route path="dashboard" element={<Dashboard/>} />
         <Route path="fund-transfer" element={<FundTransfer />}></Route>
         <Route path="balance" element={<Balance />} />
         <Route path="profile" element={<Profile />} />
+        <Route path="login" element={<Login />}/>
+        <Route path="register" element={<Register />} />
         <Route path="fund-transfer/self-transfer" element={<DepositForm />} />
         <Route
           path="fund-transfer/other-transfer"
