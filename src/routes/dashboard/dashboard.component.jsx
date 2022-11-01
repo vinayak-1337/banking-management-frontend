c5e18361686463cc420e624a57076e7c35171ed1
vinayak <vinayak@gkmit.co>
2022-11-01 22:41:18 +0530

    Fix dashboard

--- src/routes/dashboard/dashboard.component.jsx
+++ src/routes/dashboard/dashboard.component.jsx
@@ -1,11 +1,13 @@
-import Profile from "../../components/profile/profile.components";
-import DashboardImage from "../../assets/dashboard.png"
+import DashboardImage from "../../assets/dashboard.png";
 
-import "./dashboard.styles.css"
+import "./dashboard.styles.css";
+import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
-  return (<div className="dashboard-container">
-    <img src={DashboardImage} alt="dashboard" />
-    <Profile/>
-  </div>);
+  return (
+    <div className="dashboard-container">
+      <img src={DashboardImage} alt="dashboard" />
+      <Outlet />
+    </div>
+  );
 }
