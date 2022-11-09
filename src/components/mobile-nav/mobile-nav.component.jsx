4b7d083cde49ca15781f0a230c61b6564d15595a
vinayak <vinayak@gkmit.co>
2022-11-09 17:10:45 +0530

    Fix mobile nav bar

--- src/components/mobile-nav/mobile-nav.component.jsx
+++ src/components/mobile-nav/mobile-nav.component.jsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./mobile-nav.styles.css";
 
 export default function MobileNav({ show, onClose }) {
-  if(!show) return null
+  let navStyle = show ? "show-overlay" : "";
+  const [isExpand, setIsExpand] = useState(false);
+
+  const handleExpand = ()=> {
+    setIsExpand(prev=>!prev)
+  }
+
   return (
-    <div id="myNav" className="overlay">
+    <div id="myNav" className={`overlay ${navStyle}`}>
       <button className="closebtn" onClick={onClose}>
         &times;
       </button>
       <div className="overlay-content">
-        <Link to="dashboard/self-transfer" onClick={onClose}>Self transfer</Link>
-        <Link to="dashboard/other-transfer" onClick={onClose}>Other bank transfer</Link>
-        <Link to="/dashboard" onClick={onClose}>My Profile</Link>
-        <Link to="dashboard/transaction" onClick={onClose}>Transactions</Link>
+        <div className="mobile-fund-transfer" onClick={handleExpand}>
+          Fund Transfer
+          <div className={`mft-content ${isExpand ? "mft-expand":""}`}>
+            <Link to="dashboard/self-transfer" onClick={onClose}>
+              Self transfer
+            </Link>
+            <Link to="dashboard/other-transfer" onClick={onClose}>
+              Other bank transfer
+            </Link>
+          </div>
+        </div>
+        <Link to="/dashboard" onClick={onClose}>
+          My Profile
+        </Link>
+        <Link to="dashboard/transaction" onClick={onClose}>
+          Transactions
+        </Link>
       </div>
     </div>
   );
